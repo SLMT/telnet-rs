5b82b5a77aac9b81f5bb46024c65c8f7d44b93dc
SLMT <sam123456777@gmail.com>
2017-10-14 00:06:07 +0800

    Start to process telnet commands

--- src/event.rs
+++ src/event.rs
@@ -1,4 +1,9 @@
 
+#[derive(Debug)]
 pub enum TelnetEvent<'a> {
-    DataReceived(&'a [u8])
+    DataReceived(&'a [u8]),
+    UnknownIAC(u8),
+    Negotiation(u8),
+    Subnegotiation(u8, &'a [u8]),
+    Nothing
 }
--- src/net.rs
+++ src/net.rs
@@ -6,9 +6,30 @@ use std::net::{TcpStream, ToSocketAddrs};
 use option::TelnetOption;
 use event::TelnetEvent;
 
+const BYTE_IAC: u8 = 255;   // interpret as command:
+const BYTE_DONT: u8 = 254;  // you are not to use option
+const BYTE_DO: u8 = 253;    // please, you use option
+const BYTE_WONT: u8 = 252;  // I won't use option
+const BYTE_WILL: u8 = 251;  // I will use option
+const BYTE_SB: u8 = 250;    // interpret as subnegotiation
+const BYTE_SE: u8 = 240;    // end sub negotiation
+
+enum ProcessState {
+    NormalData,
+    IAC,
+    SB,
+    SBData(u8, usize),    // (option byte, start location of option data)
+    SBDataIAC(u8, usize), // (option byte, start location of option data)
+    WILL, WONT,
+    DO, DONT
+}
+
 pub struct TelnetStream {
     stream: TcpStream,
     options: Vec<TelnetOption>,
+    state: ProcessState,
+
+    // Buffer
     buffer: Box<[u8]>,
     next_start: usize, // the position next start to read
     buffered_size: usize
@@ -22,6 +43,7 @@ impl TelnetStream {
         TelnetStream {
             stream: stream,
             options: options,
+            state: ProcessState::NormalData,
             buffer: vec![0; actual_size].into_boxed_slice(),
             next_start: 0,
             buffered_size: 0
@@ -47,12 +69,113 @@ impl TelnetStream {
             }
         }
 
-        // Return the data
-        let start = self.next_start;
-        let end = self.buffered_size;
-        let event = TelnetEvent::DataReceived(&self.buffer[start .. end]);
-        self.next_start = self.buffered_size;
-        return Ok(event);
+        // Process the buffered bytes
+        return Ok(self.process());
+    }
+
+    // TODO: Unhandled cases:
+    // IAC IAC
+    // IAC SB IAC
+    fn process<'a>(&'a mut self) -> TelnetEvent<'a> {
+        let mut current = self.next_start;
+
+        while current < self.buffered_size {
+            // Gather a byte
+            let byte = self.buffer[current];
+
+            // Process the byte
+            match self.state {
+                // Normal Data
+                ProcessState::NormalData => {
+                    if byte == BYTE_IAC {
+                        // The following bytes will be commands
+
+                        // Update the states
+                        self.state = ProcessState::IAC;
+                        let start = self.next_start;
+                        self.next_start = current + 1;
+
+                        // Send the data before this byte
+                        if current > start {
+                            return TelnetEvent::DataReceived(
+                                &self.buffer[start .. current]);
+                        }
+                    } else if current == self.buffered_size - 1 {
+                        // It reaches the end of the buffer
+                        let event = TelnetEvent::DataReceived(
+                            &self.buffer[self.next_start .. self.buffered_size]);
+                        self.next_start = current + 1;
+
+                        return event;
+                    }
+                },
+
+                // Telnet Commands
+                ProcessState::IAC => {
+                    match byte {
+                        // Negotiation Commands
+                        BYTE_WILL => self.state = ProcessState::WILL,
+                        BYTE_WONT => self.state = ProcessState::WONT,
+                        BYTE_DO => self.state = ProcessState::DO,
+                        BYTE_DONT => self.state = ProcessState::DONT,
+                        // Subnegotiation
+                        BYTE_SB => self.state = ProcessState::SB,
+                        // Unknown IAC commands
+                        _ => {
+                            self.state = ProcessState::NormalData;
+                            self.next_start = current + 1;
+                            return TelnetEvent::UnknownIAC(byte);
+                        }
+                    }
+                },
+
+                // Negotiation
+                ProcessState::WILL | ProcessState::WONT |
+                        ProcessState::DO | ProcessState::DONT => {
+                     self.state = ProcessState::NormalData;
+                     self.next_start = current + 1;
+                     // TODO: We should handle each case separately
+                     return TelnetEvent::Negotiation(byte);
+                 },
+
+                // Start subnegotiation
+                ProcessState::SB => {
+                    self.state = ProcessState::SBData(byte, current + 1);
+                },
+
+                // Subnegotiation's data
+                ProcessState::SBData(opt, data_start) => {
+                    if byte == BYTE_IAC {
+                        self.state = ProcessState::SBDataIAC(opt, data_start);
+                    }
+                },
+
+                // IAC inside Subnegotiation's data
+                ProcessState::SBDataIAC(opt, data_start) => {
+                    if byte == BYTE_SE {
+                        // Update state
+                        self.state = ProcessState::NormalData;
+                        self.next_start = current + 1;
+
+                        // Return the option
+                        let data_end = current - 1;
+                        // TODO: We should handle each case separately
+                        return TelnetEvent::Subnegotiation(opt,
+                                &self.buffer[data_start .. data_end]);
+                    }
+                },
+
+                // TODO: others
+                _ => {
+
+                }
+            }
+
+            // Move to the next byte
+            current += 1;
+        }
+
+        return TelnetEvent::Nothing;
     }
 }
 
