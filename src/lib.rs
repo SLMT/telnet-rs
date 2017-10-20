860a2c138a512406e52617166a6598b629126368
SLMT <sam123456777@gmail.com>
2017-10-20 21:00:38 +0800

    Handle IAC escaping

--- src/lib.rs
+++ src/lib.rs
@@ -38,6 +38,8 @@ pub struct TelnetConnection {
     // Buffer
     buffer: Box<[u8]>,
     buffered_size: usize,
+    process_buffer: Box<[u8]>,
+    process_buffered_size: usize,
 
     // Negotiation
     negotiation_sm: NegotiationSM
@@ -55,6 +57,8 @@ impl TelnetConnection {
             event_queue: TelnetEventQueue::new(),
             buffer: vec![0; actual_size].into_boxed_slice(),
             buffered_size: 0,
+            process_buffer: vec![0; actual_size].into_boxed_slice(),
+            process_buffered_size: 0,
             negotiation_sm: NegotiationSM::new()
         }
     }
@@ -109,13 +113,15 @@ impl TelnetConnection {
 
                         // Send the data before this byte
                         if current > data_start {
-                            self.event_queue.push_event(TelnetEvent::DataReceived(
-                                Box::from(&self.buffer[data_start .. current])));
+                            let data_end = current;
+                            let data = self.copy_buffered_data(data_start, data_end);
+                            self.event_queue.push_event(TelnetEvent::DataReceived(data));
                         }
                     } else if current == self.buffered_size - 1 {
                         // It reaches the end of the buffer
-                        self.event_queue.push_event(TelnetEvent::DataReceived(
-                            Box::from(&self.buffer[data_start .. self.buffered_size])));
+                        let data_end = self.buffered_size;
+                        let data = self.copy_buffered_data(data_start, data_end);
+                        self.event_queue.push_event(TelnetEvent::DataReceived(data));
                     }
                 },
 
@@ -129,6 +135,20 @@ impl TelnetConnection {
                         BYTE_DONT => state = ProcessState::Dont,
                         // Subnegotiation
                         BYTE_SB => state = ProcessState::SB,
+                        // Escaping
+                        // TODO: Write a test case for this
+                        BYTE_IAC => {
+                            // Copy the data to the process buffer
+                            self.append_data_to_proc_buffer(data_start, current - 1);
+
+                            // Add escaped IAC
+                            self.process_buffer[self.process_buffered_size] = BYTE_IAC;
+                            self.process_buffered_size += 1;
+
+                            // Update the state
+                            state = ProcessState::NormalData;
+                            data_start = current + 1;
+                        },
                         // Unknown IAC commands
                         _ => {
                             state = ProcessState::NormalData;
@@ -195,20 +215,42 @@ impl TelnetConnection {
                     if byte == BYTE_IAC {
                         state = ProcessState::SBDataIAC(opt, data_start);
                     }
+
+                    // XXX: We need to consider the case that a SB Data
+                    // sequence may exceed this buffer
                 },
 
                 // IAC inside Subnegotiation's data
                 ProcessState::SBDataIAC(opt, sb_data_start) => {
-                    if byte == BYTE_SE {
-                        // Update state
-                        state = ProcessState::NormalData;
-                        data_start = current + 1;
+                    match byte {
+                        // The end of subnegotiation
+                        BYTE_SE => {
+                            // Update state
+                            state = ProcessState::NormalData;
+                            data_start = current + 1;
 
-                        // Return the option
-                        let sb_data_end = current - 1;
-                        self.event_queue.push_event(TelnetEvent::SBReceived(
-                            opt, Box::from(&self.buffer[sb_data_start .. sb_data_end])));
+                            // Return the option
+                            let sb_data_end = current - 1;
+                            let data = self.copy_buffered_data(sb_data_start, sb_data_end);
+                            self.event_queue.push_event(TelnetEvent::SBReceived(opt, data));
+                        },
+                        // Escaping
+                        // TODO: Write a test case for this
+                        BYTE_IAC => {
+                            // Copy the data to the process buffer
+                            self.append_data_to_proc_buffer(sb_data_start, current - 1);
+
+                            // Add escaped IAC
+                            self.process_buffer[self.process_buffered_size] = BYTE_IAC;
+                            self.process_buffered_size += 1;
+
+                            // Update the state
+                            state = ProcessState::SBData(opt, current + 1);
+                        },
+                        _ => {}
                     }
+
+                    // TODO: Error: unexpected byte after IAC inside SB
                 },
 
                 // TODO: others
@@ -220,5 +262,31 @@ impl TelnetConnection {
             // Move to the next byte
             current += 1;
         }
+    }
+
+    // Copy the data to the process buffer
+    fn append_data_to_proc_buffer(&mut self, data_start: usize, data_end: usize) {
+        let data_length = data_end - data_start;
+        let dst_start = self.process_buffered_size;
+        let dst_end = self.process_buffered_size + data_length;
+        let dst = &mut self.process_buffer[dst_start .. dst_end];
+        dst.copy_from_slice(&self.buffer[data_start .. data_end]);
+        self.process_buffered_size += data_length;
+    }
+
+    fn copy_buffered_data(&mut self, data_start: usize, data_end: usize) -> Box<[u8]> {
+        let data = if self.process_buffered_size > 0 {
+            // Copy the data to the process buffer
+            self.append_data_to_proc_buffer(data_start, data_end);
+
+            let pbe = self.process_buffered_size;
+            self.process_buffered_size = 0;
+
+            &self.process_buffer[0 .. pbe]
+        } else {
+            &self.buffer[data_start .. data_end]
+        };
+
+        Box::from(data)
     }
 }
