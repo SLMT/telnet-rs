06cc1f4d87e4ea5e17f777139b96f6ac28a23151
SLMT <sam123456777@gmail.com>
2017-12-22 12:09:28 +0800

    Redesgin API

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,41 @@ pub use option::{TelnetOption, TelnetOptionConfigs};
 pub use event::TelnetEvent;
 pub use negotiation::NegotiationAction;
 pub use connection::TelnetConnection;
+
+pub struct Telnet {
+
+}
+
+impl Telnet {
+    pub fn connect<A: ToSocketAddrs>(addr: A, buf_size: usize) -> io::Result<Telnet> {
+
+    }
+
+    pub fn read(&mut self) -> TelnetEvent {
+
+    }
+
+    pub fn read_timeout(&mut self, timeout: Duration) -> TelnetEvent {
+
+    }
+
+    pub fn read_nonblocking(&mut self) -> TelnetEvent {
+
+    }
+
+    pub fn write(&mut self, data: &[u8]) {
+        
+    }
+
+    pub fn negotiate(&self, action: NegotiationAction, opt: TelnetOption) {
+
+    }
+
+    pub fn subnegotiate(&self, action: NegotiationAction, opt: TelnetOption, data: &[u8]) {
+
+    }
+
+    pub fn set_debug_logging(&mut self, enabled: bool) {
+
+    }
+}
