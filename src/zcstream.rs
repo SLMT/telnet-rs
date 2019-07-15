221bb441728971e5b073522bda797ce49ef88962
Yehoshua Pesach Wallach <yehoshuapw@gmail.com>
2019-07-15 22:07:19 +0300

    Added MCCP2 basic usage to telnet struct (out of feature switches)

--- src/zcstream.rs
+++ src/zcstream.rs
@@ -1,5 +1,6 @@
 use stream::Stream;
 
+/// Stream with ability to be upgraded to zlib stream.
 pub trait ZCStream: Stream {
     /// Begin zlib decompression on downstream. Ignored if already enabled.
     fn begin_zlib(&mut self);
